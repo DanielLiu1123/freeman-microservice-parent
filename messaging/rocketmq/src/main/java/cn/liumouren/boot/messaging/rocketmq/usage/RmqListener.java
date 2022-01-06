4d1706952539023187f5b3277663698980304363
Freeman Lau <llw599502537@gmail.com>
2022-01-07 01:55:25 +0800

    fix: test

--- messaging/rocketmq/src/main/java/cn/liumouren/boot/messaging/rocketmq/usage/RmqListener.java
+++ messaging/rocketmq/src/main/java/cn/liumouren/boot/messaging/rocketmq/usage/RmqListener.java
@@ -1,29 +1,28 @@
-package cn.liumouren.boot.messaging.rocketmq.usage;
-
-import cn.liumouren.boot.messaging.rocketmq.OrderEvent;
-import org.apache.rocketmq.spring.annotation.ConsumeMode;
-import org.apache.rocketmq.spring.annotation.MessageModel;
-import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
-import org.apache.rocketmq.spring.core.RocketMQListener;
-import org.springframework.stereotype.Component;
-
-/**
- *
- *
- * @author freeman
- * @date 2021/12/28 21:31
- */
-@Component
-@RocketMQMessageListener(
-        consumerGroup = "order_group",
-        topic = "order",
-        selectorExpression = "tag01 || tag02",
-        consumeMode = ConsumeMode.CONCURRENTLY,
-        messageModel = MessageModel.CLUSTERING
-)
-public class RmqListener implements RocketMQListener<OrderEvent> {
-    @Override
-    public void onMessage(OrderEvent message) {
-
-    }
-}
+//package cn.liumouren.boot.messaging.rocketmq.usage;
+//
+//import org.apache.rocketmq.spring.annotation.ConsumeMode;
+//import org.apache.rocketmq.spring.annotation.MessageModel;
+//import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
+//import org.apache.rocketmq.spring.core.RocketMQListener;
+//import org.springframework.stereotype.Component;
+//
+///**
+// *
+// *
+// * @author freeman
+// * @date 2021/12/28 21:31
+// */
+//@Component
+//@RocketMQMessageListener(
+//        consumerGroup = "order_group",
+//        topic = "order",
+//        selectorExpression = "tag01 || tag02",
+//        consumeMode = ConsumeMode.CONCURRENTLY,
+//        messageModel = MessageModel.CLUSTERING
+//)
+//public class RmqListener implements RocketMQListener<SomeEvent> {
+//    @Override
+//    public void onMessage(SomeEvent message) {
+//
+//    }
+//}
