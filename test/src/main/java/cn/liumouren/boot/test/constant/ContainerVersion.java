72f819bdc9d2cb16d6f203b4cb5fa23f3a3158ef
Freeman Lau <llw599502537@gmail.com>
2022-01-12 01:45:27 +0800

    feat: New module test

---
+++ test/src/main/java/cn/liumouren/boot/test/constant/ContainerVersion.java
@@ -0,0 +1,19 @@
+package cn.liumouren.boot.test.constant;
+
+/**
+ * Container version for testcontainers.
+ *
+ * @author freeman
+ * @date 2022/1/12 00:44
+ */
+public interface ContainerVersion {
+
+    // TODO complete it!
+
+    String MYSQL = "mysql:8";
+
+    String REDIS = "redis:6.2-alpine";
+
+    String MONGO = "mongo:5.0-focal";
+
+}
---
+++ test/src/test/java/cn/liumouren/boot/test/constant/ContainerTest.java
@@ -0,0 +1,44 @@
+package cn.liumouren.boot.test.constant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static cn.liumouren.boot.test.constant.ContainerVersion.MONGO;
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ *
+ *
+ * @author freeman
+ * @date 2022/1/12 00:54
+ */
+@SpringBootTest
+@SpringBootApplication
+@Testcontainers
+public class ContainerTest {
+
+    @Container
+    static MongoDBContainer mongo = new MongoDBContainer(MONGO);
+
+    @DynamicPropertySource
+    static void redisProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.port", () -> mongo.getFirstMappedPort());
+    }
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @Test
+    public void mongodb_is_ready() {
+        assertThat(mongoTemplate).isNotNull();
+    }
+
+}
